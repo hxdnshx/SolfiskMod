5c57419cc4f44a69b056490a4d2e5e0a56914e9d
hxdnshx <recollectionforgot@gmail.com>
2014-04-04 19:17:13 +0800

    Ver5.35 -> 5.38

--- MainWnd.hpp
+++ MainWnd.hpp
@@ -4,7 +4,7 @@
 
 // マンボウ
 #define WINDOW_CLASS _T("Solfisk")
-#define WINDOW_TEXT  _T("綺録帖 Rev.5.35")
+#define WINDOW_TEXT  _T("綺録帖 Rev.5.38")
 
 bool MainWindow_Initialize();
 bool MainWindow_PreTranslateMessage(MSG &msg);
